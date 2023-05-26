ff9cdf5d51bcd280e8378a6c4b32f45026f92956
AnnaErs <annaershova02@gamil.com>
2023-05-26 15:41:03 +0500

    feat: SUEFA-3 [FRONTEND] Сверсать главную

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import MainPage from "@components/pages/MainPage/MainPage";
 
 export default function Home() {
     return (
-        <main className="flex flex-col items-center justify-between">
+        <main className="flex flex-col items-center">
             <MainPage />
         </main>
     );
